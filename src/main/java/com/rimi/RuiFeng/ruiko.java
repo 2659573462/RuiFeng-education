2e1eaaded2efbde6bc366e48f5353d5233efab80
chenjin <2659573462@qq.com>
2019-11-05 10:51:05 +0800

    测试0.1

---
+++ src/main/java/com/rimi/RuiFeng/ruiko.java
@@ -0,0 +1,11 @@
+package com.rimi.RuiFeng;
+
+/**
+ * @author chenjin
+ * @date 2019/11/5 10:49
+ */
+public class ruiko {
+    public static void main(String[] args) {
+        System.out.println("测试厂库0.1");
+    }
+}
