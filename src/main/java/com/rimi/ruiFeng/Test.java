4a5883b11ff5459a4d67374c166fc53745d2a4b5
chenjin <2659573462@qq.com>
2019-11-05 10:54:01 +0800

    测试0.1

--- src/main/java/com/rimi/ruiFeng/Test.java
+++ src/main/java/com/rimi/ruiFeng/Test.java
@@ -1,10 +1,10 @@
-package com.rimi.RuiFeng;
+package com.rimi.ruiFeng;
 
 /**
  * @author chenjin
  * @date 2019/11/5 10:49
  */
-public class ruiko {
+public class Test {
     public static void main(String[] args) {
         System.out.println("测试厂库0.1");
     }
